a77d4b5bfc388331ad28814c262f22a69bbd40e3
Farhad K <farhadk8552@gmail.com>
2024-03-17 16:56:25 +0000

    Event Database connection

--- Controller/Admin/Events/AddEvent.php
+++ Controller/Admin/Events/AddEvent.php
@@ -4,24 +4,35 @@
 require_once("../../../Model/Configurations/db.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $eventName = $_POST['event-Id'];
+    $eventName = $_POST['event_name'];
     $eventLocation = $_POST['event_location'];
     $eventDate = $_POST['event_date'];
     $eventOrganizer = $_POST['event_organizer'];
+    $deviceID = 1; 
+    $organizerID = 1; 
+    $eventTime = date(""); 
+    $eventDescription = ""; 
 
-    $insertQuery = "INSERT INTO Events (EventName, EventLocation, EventDate) VALUES (?, ?, ?)";
+    $insertQuery = "INSERT INTO Events (EventName, EventLocation, EventDate, OrganizerID, EventTime, EventDescription, DeviceID) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $statement = mysqli_prepare($con, $insertQuery);
-    mysqli_stmt_bind_param($statement, "sss", $eventName, $eventLocation, $eventDate);
-    
-    if (mysqli_stmt_execute($statement)) {
-        echo "Event Add Successfully";
+
+    if ($statement) {
+        mysqli_stmt_bind_param($statement, "sssssss", $eventName, $eventLocation, $eventDate, $organizerID, $eventTime, $eventDescription, $deviceID);
+        
+        if (mysqli_stmt_execute($statement)) {
+            echo "Event Added Successfully";
+        } else {
+            echo "Error" . mysqli_stmt_error($statement);
+        }
+
+        mysqli_stmt_close($statement);
     } else {
-        echo "Error updating record: " . $stmt->error;
+        echo "Error" . mysqli_error($con);
     }
 
-    mysqli_stmt_close($statement);
     mysqli_close($con);
 } else {
     header('Location: ../Admin/Events.php');
+    exit();
 }
 ?>
